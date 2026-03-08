e20067e46a953e283b4077a85a6e869d60186944
maximilian-hausers <maximilian-hausers@proton.me>
2026-03-08 17:47:32 +0000

    optimize setting the local storage

--- src/load_localStorage.js
+++ src/load_localStorage.js
@@ -1,12 +1,16 @@
 const { DEFAULT_STREAMING_SERVER_URL } = require('./common/CONSTANTS');
 
 async function initialize() {
-    localStorage.setItem("streaming_server_urls", JSON.stringify({
-        "uid": null,
-        "items": {
-            [DEFAULT_STREAMING_SERVER_URL]: "2099-12-31T23:59:59.999Z"
-        }
-    }));
+    const streamingServerUrls = localStorage.getItem("streaming_server_urls");
+    if (!streamingServerUrls || !JSON.parse(streamingServerUrls).items[DEFAULT_STREAMING_SERVER_URL]) {
+        localStorage.setItem("streaming_server_urls", JSON.stringify({
+            "uid": null,
+            "items": {
+                [DEFAULT_STREAMING_SERVER_URL]: "2099-12-31T23:59:59.999Z"
+            }
+        }));
+        window.location.reload();
+    }
 }
 
 initialize();
